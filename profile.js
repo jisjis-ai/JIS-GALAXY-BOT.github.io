c3926b04048d781b432316567ee6daec46ce2156
jisjis-ai <125311127+jisjis-ai@users.noreply.github.com>
2024-04-23 12:36:33 +0200

    Update profile.js

--- profile.js
+++ profile.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const button1 = document.getElementById("button1");
   const button2 = document.getElementById("button2");
   const button3 = document.getElementById("button3");
+  const button4 = document.getElementById("button4");
 
   logoutButton.addEventListener("click", function () {
     // Simular cierre de sesión: redirigir a la página de inicio de sesión
@@ -21,4 +22,12 @@ document.addEventListener("DOMContentLoaded", function () {
   button3.addEventListener("click", function () {
     window.location.href = "olabet.html";
   });
+  button4.addEventListener("click", function () {
+    window.location.href = "bets.html";
+  });
+  button5.addEventListener("click", function () {
+    window.location.href = "way.html";
+  });
+  
 });
+  
